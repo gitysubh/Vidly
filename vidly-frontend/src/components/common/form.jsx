b446aaf0734bd91e65b277e8628194425e3cc71c
gitysubh <pal.subhashis62@gmail.com>
2020-08-20 09:17:04 +0530

    Refactor the form as reusuable component

---
+++ vidly-frontend/src/components/common/form.jsx
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import Joi from "joi-browser";
+import Input from "./input";
+
+class Form extends Component {
+  state = {
+    data: {},
+    errors: {},
+  };
+
+  handleChange = (e) => {
+    const el = e.target;
+    const { data, errors } = this.state;
+    data[el.name] = el.value;
+    delete errors[el.name];
+    this.setState({ data, errors });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const errors = this.validateForm();
+    if (errors && Object.keys(errors).length) {
+      this.setState({ errors: errors || {} });
+      return;
+    } else this.doSubmit();
+  };
+
+  validateForm = (e) => {
+    const options = { abortEarly: false };
+    const { error: validatorErrors } = Joi.validate(
+      this.state.data,
+      this.schema,
+      options
+    );
+    if (!validatorErrors) return null;
+
+    let errors = {};
+    validatorErrors.details.map((errorItem) => {
+      if (errors.hasOwnProperty(errorItem.path[0])) return;
+      errors = { ...errors, ...{ [errorItem.path[0]]: errorItem.message } };
+    });
+    return errors;
+  };
+
+  validateField = (e) => {
+    const el = e.target;
+    const schema = { [el.name]: this.schema[el.name] };
+    if (schema) {
+      const { error: validatorErrors } = Joi.validate(
+        { [el.name]: el.value },
+        schema
+      );
+      if (validatorErrors) {
+        const error = { [el.name]: validatorErrors.details[0].message };
+        this.setState({ errors: { ...this.state.errors, ...error } });
+      }
+    }
+  };
+
+  renderInput({ name, label, type = "text", placeholder, ...rest }) {
+    const { data, errors } = this.state;
+
+    return (
+      <Input
+        label={label}
+        name={name}
+        value={data[name] || ""}
+        onChange={this.handleChange}
+        errorText={errors[name]}
+        placeholder={placeholder}
+        validator={this.validateField}
+        rest
+      />
+    );
+  }
+
+  renderSubmitButton(label) {
+    const disabled = !!Object.keys(this.state.errors).length;
+
+    return (
+      <button type="submit" className="btn btn-primary" disabled={disabled}>
+        {label}
+      </button>
+    );
+  }
+}
+
+export default Form;
--- vidly-frontend/src/components/login.jsx
+++ vidly-frontend/src/components/login.jsx
@@ -1,100 +1,37 @@
 import React, { Component } from "react";
 import Joi from "joi-browser";
 import Input from "./common/input";
+import Form from "./common/form";
 
-class Login extends Component {
-  state = {
-    data: {
-      email: "",
-      password: "",
-    },
-    errors: {},
-  };
-
+class Login extends Form {
   schema = {
     email: Joi.string().email().required().label("Email"),
     password: Joi.string().required().min(6).max(16).label("Password"),
   };
 
-  handleChange = (e) => {
-    const el = e.target;
-    const { data, errors } = this.state;
-    data[el.name] = el.value;
-    errors[el.name] = "";
-    this.setState({ data, errors });
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const errors = this.validateForm();
-    if (errors && Object.keys(errors).length) {
-      this.setState({ errors: errors || {} });
-      return;
-    } else alert("success");
-  };
-
-  validateForm = (e) => {
-    const options = { abortEarly: false };
-    const { error: validatorErrors } = Joi.validate(
-      this.state.data,
-      this.schema,
-      options
-    );
-    if (!validatorErrors) return null;
-
-    let errors = {};
-    validatorErrors.details.map((errorItem) => {
-      if (errors.hasOwnProperty(errorItem.path[0])) return;
-      errors = { ...errors, ...{ [errorItem.path[0]]: errorItem.message } };
-    });
-    return errors;
-  };
-
-  validateField = (e) => {
-    const el = e.target;
-    const schema = { [el.name]: this.schema[el.name] };
-    if (schema) {
-      const { error: validatorErrors } = Joi.validate(
-        { [el.name]: el.value },
-        schema
-      );
-      if (validatorErrors) {
-        const error = { [el.name]: validatorErrors.details[0].message };
-        this.setState({ errors: { ...this.state.errors, ...error } });
-      }
-    }
-  };
+  doSubmit() {
+    alert("success");
+  }
 
   render() {
-    const { email, password } = this.state.data;
-    const { errors } = this.state;
-
     return (
-      <form onSubmit={this.handleSubmit}>
-        <Input
-          label="Enter Email"
-          name="email"
-          value={email}
-          onChange={this.handleChange}
-          errorText={errors.email}
-          placeholder="Enter your email"
-          validator={this.validateField}
-        />
-        <Input
-          label="Enter Password"
-          name="password"
-          value={password}
-          onChange={this.handleChange}
-          errorText={errors.password}
-          type="password"
-          placeholder="Enter your password"
-          validator={this.validateField}
-        />
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput({
+            name: "email",
+            label: "Email",
+            placeholder: "Enter your email",
+          })}
+          {this.renderInput({
+            name: "password",
+            label: "Password",
+            placeholder: "Enter your password",
+          })}
 
-        <button type="submit" className="btn btn-primary">
-          Submit
-        </button>
-      </form>
+          {this.renderSubmitButton("Login")}
+        </form>
+      </div>
     );
   }
 }
