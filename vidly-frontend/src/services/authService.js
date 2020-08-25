e902a06fe69fdc2d125007d5dd00ee6170e552ac
Subhashis Pal <subhashis.pal@chatlead.com>
2020-08-26 00:13:57 +0530

    Call peotected apis with jwt

--- vidly-frontend/src/services/authService.js
+++ vidly-frontend/src/services/authService.js
@@ -4,38 +4,44 @@ import jwtDecode from "jwt-decode";
 const tokenKey = "auth";
 const apiEndpoint = "/auth";
 
+http.setAuthToken(getAuthToke());
+
+function getAuthToke() {
+    return localStorage.getItem(tokenKey);
+}
+
 function setToken(token) {
-  localStorage.setItem(tokenKey, token);
+    localStorage.setItem(tokenKey, token);
 }
 
 export async function login(username, password) {
-  const { data: jwt } = await http.post(apiEndpoint, {
-    email: username,
-    password: password,
-  });
-  setToken(jwt);
+    const { data: jwt } = await http.post(apiEndpoint, {
+        email: username,
+        password: password,
+    });
+    setToken(jwt);
 }
 
 export function loginWithJwt(jwt) {
-  setToken(jwt);
+    setToken(jwt);
 }
 
 export function logout() {
-  localStorage.removeItem(tokenKey);
+    localStorage.removeItem(tokenKey);
 }
 
 export function getCurrentUser() {
-  try {
-    const jwt = localStorage.getItem(tokenKey);
-    return jwtDecode(jwt);
-  } catch (error) {
-    return null;
-  }
+    try {
+        const jwt = localStorage.getItem(tokenKey);
+        return jwtDecode(jwt);
+    } catch (error) {
+        return null;
+    }
 }
 
 export default {
-  login,
-  logout,
-  loginWithJwt,
-  getCurrentUser,
+    login,
+    logout,
+    loginWithJwt,
+    getCurrentUser,
 };
--- vidly-frontend/src/services/httpService.js
+++ vidly-frontend/src/services/httpService.js
@@ -1,22 +1,28 @@
 import axios from "axios";
 import { toast } from "react-toastify";
+import { func } from "prop-types";
 
 const baseURL = "http://localhost:3900/api";
 const instance = axios.create({ baseURL });
 
 instance.interceptors.response.use(null, (data) => {
-  const expectedError =
-    data.response && data.response.status >= 400 && data.response.status < 500;
-  if (!expectedError) {
-    toast.error("An unknown error occurred");
-  }
+    const expectedError =
+        data.response && data.response.status >= 400 && data.response.status < 500;
+    if (!expectedError) {
+        toast.error("An unknown error occurred");
+    }
 
-  return Promise.reject(data);
+    return Promise.reject(data);
 });
 
+function setAuthToken(jwt) {
+    instance.defaults.headers.common['x-auth-token'] = jwt;
+}
+
 export default {
-  get: instance.get,
-  post: instance.post,
-  delete: instance.delete,
-  put: instance.put,
+    get: instance.get,
+    post: instance.post,
+    delete: instance.delete,
+    put: instance.put,
+    setAuthToken
 };
